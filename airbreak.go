22b7c0ffb22a99a8b8ecefae9f89efa2fcffed31
Tobias Lütke <tobi@shopify.com>
2012-05-07 21:23:56 -0400

    Made the whole thing async, no need to wait for responses

--- airbreak.go
+++ airbreak.go
@@ -8,12 +8,128 @@ import (
   "os"
   "text/template"
   "net/http"
+  "sync"
 )
 
 var ApiKey string =  ""  
 var Endpoint string = "https://airbreak.io/notifier_api/v2/notices.xml"
+var tmpl = template.Must(template.New("error").Parse(source))
+var Verbose = false
 
-const source = `<?xml version="1.0" encoding="UTF-8"?>
+var (
+  badResponse = errors.New("Bad response")
+  apiKeyMissing = errors.New("Please set the airbreak.ApiKey before doing calls")
+  dunno     = []byte("???")
+  centerDot = []byte("·")
+  dot       = []byte(".")
+)
+
+type Line struct { 
+  Function string
+  File string
+  Line int
+}
+
+// stack implements Stack, skipping N frames
+func stacktrace(skip int) (lines []Line) {
+  for i := skip; ; i++ { 
+    pc, file, line, ok := runtime.Caller(i)
+    if !ok {
+      break
+    }
+
+    item := Line{ string(function(pc)), string(file), line}
+    lines = append(lines, item )
+  }
+  return
+}
+
+var channel chan map[string]interface{} 
+var once sync.Once 
+
+// function returns, if possible, the name of the function containing the PC.
+func function(pc uintptr) []byte {
+  fn := runtime.FuncForPC(pc)
+  if fn == nil {
+    return dunno
+  }
+  name := []byte(fn.Name())
+  // The name includes the path name to the package, which is unnecessary
+  // since the file name is already included.  Plus, it has center dots.
+  // That is, we see
+  //  runtime/debug.*T·ptrmethod
+  // and want
+  //  *T.ptrmethod
+  if period := bytes.Index(name, dot); period >= 0 {
+    name = name[period+1:]
+  }
+  name = bytes.Replace(name, centerDot, dot, -1)
+  return name
+}
+
+func initChannel() { 
+  channel = make(chan map[string]interface{}, 100)
+  go worker()
+}
+
+func worker() {
+  for params := range channel {
+    buffer := bytes.NewBufferString("")
+
+    if err := tmpl.Execute(buffer, params); err != nil {    
+      log.Printf("Airbreak error: %s", err)
+      continue
+    }
+
+    response, err := http.Post(Endpoint, "text/xml", buffer)
+    response.Body.Close()
+
+    if err != nil {    
+      log.Printf("Airbreak error: %s", err)
+      continue
+    }
+
+    if Verbose {
+      log.Printf("Airbreak post: %s", params["Error"])
+    }
+
+  }
+
+  return
+}
+
+func ErrorRequest(e error, request *http.Request) error {
+  once.Do(initChannel)
+
+  if ApiKey == "" {
+    return apiKeyMissing
+  }
+
+  params := map[string]interface{} { 
+    "Error": e, 
+    "ApiKey": ApiKey, 
+    "ErrorName": e.Error(),
+    "Request": request,
+  }
+
+  pwd, err := os.Getwd()
+  if err == nil {
+    params["Pwd"] = pwd
+  }
+
+  params["Backtrace"] = stacktrace(3)
+
+  channel <- params
+  return nil
+}
+
+func Error(e error) error {
+  return ErrorRequest(e, nil)
+}
+
+
+const source = `
+<?xml version="1.0" encoding="UTF-8"?>
 <notice version="2.0">
   <api-key>{{ .ApiKey }}</api-key>
   <notifier>
@@ -36,7 +152,6 @@ const source = `<?xml version="1.0" encoding="UTF-8"?>
     <component/>
     <action/>
     <cgi-data>
-      <var key="SERVER_NAME">example.org</var>
     </cgi-data>
   </request>
   {{ end }}  
@@ -45,105 +160,3 @@ const source = `<?xml version="1.0" encoding="UTF-8"?>
     <project-root>{{ .Pwd }}</project-root>        
   </server-environment>
 </notice>`
-
-var tmpl = template.Must(template.New("error").Parse(source))
-
-var (
-  badResponse = errors.New("Bad response")
-  apiKeyMissing = errors.New("Please set the airbreak.ApiKey before doing calls")
-  dunno     = []byte("???")
-  centerDot = []byte("·")
-  dot       = []byte(".")
-)
-
-type ExtendedError interface {
-  Message() string
-}
-
-type Line struct { 
-  Function string
-  File string
-  Line int
-}
-
-// stack implements Stack, skipping N frames
-func stacktrace(skip int) (lines []Line) {
-  for i := skip; ; i++ { 
-    pc, file, line, ok := runtime.Caller(i)
-    if !ok {
-      break
-    }
-
-    item := Line{ string(function(pc)), string(file), line}
-    lines = append(lines, item )
-  }
-
-  return
-}
-
-// function returns, if possible, the name of the function containing the PC.
-func function(pc uintptr) []byte {
-  fn := runtime.FuncForPC(pc)
-  if fn == nil {
-    return dunno
-  }
-  name := []byte(fn.Name())
-  // The name includes the path name to the package, which is unnecessary
-  // since the file name is already included.  Plus, it has center dots.
-  // That is, we see
-  //  runtime/debug.*T·ptrmethod
-  // and want
-  //  *T.ptrmethod
-  if period := bytes.Index(name, dot); period >= 0 {
-    name = name[period+1:]
-  }
-  name = bytes.Replace(name, centerDot, dot, -1)
-  return name
-}
-
-func ErrorRequest(e error, request *http.Request) error {
-  if ApiKey == "" {
-    return apiKeyMissing
-  }
-
-  params := map[string]interface{} { 
-    "Error": e, 
-    "ApiKey": ApiKey, 
-    "ErrorName": e.Error(),
-    "Request": request,
-  }
-
-  pwd, err := os.Getwd()
-  if err == nil {
-    params["Pwd"] = pwd
-  }
-
-  params["Backtrace"] = stacktrace(3)
-
-  // if err := e.(ExtendedError); err == nil {
-  //   params["ExtendedError"] = ExtendedError(err).Message()
-  // } else {
-  //   params["ExtendedError"] = e.Error()
-  // }
-
-  buffer := bytes.NewBufferString("")
-
-  if err := tmpl.Execute(buffer, params); err != nil {    
-    return err
-  }
-
-  log.Printf("%s", buffer)
-
-  response, err := http.Post(Endpoint, "text/xml", buffer)
-  defer response.Body.Close()
-
-  if response.StatusCode != 201 && response.StatusCode != 200  {
-    return badResponse
-  }
-
-  return nil
-}
-
-func Error(e error) error {
-  return ErrorRequest(e, nil)
-}
--- airbreak_test.go
+++ airbreak_test.go
@@ -6,18 +6,27 @@ import (
 //  "testing/quick"
   "net/http"
   "bytes"
+  "time"
 )
 
 func TestError(t *testing.T) {
+  Verbose = true
+  ApiKey = ""
+  Endpoint = "https://exceptions.shopify.com/notifier_api/v2/notices.xml"
   
   err := Error(errors.New("GenericFailure"))
   if err != nil {
     
     t.Error(err)
   }
+
+  time.Sleep(1e9)
 }
 
 func TestRequest(t *testing.T) {
+  Verbose = true
+  ApiKey = ""
+  Endpoint = "https://exceptions.shopify.com/notifier_api/v2/notices.xml"
 
   request, _ := http.NewRequest("GET", "/some/path?a=1", bytes.NewBufferString(""))
   
@@ -26,4 +35,6 @@ func TestRequest(t *testing.T) {
   if err != nil {    
     t.Error(err)
   }
+
+  time.Sleep(1e9)
 }
