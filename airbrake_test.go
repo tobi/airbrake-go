351a547e3d7543f5314a9a2b6b4a1dc602e92ef6
Chris Kolodin <chrisk@biba.com>
2012-10-01 16:09:34 -0700

    add Notify function which does not include stactrace

--- airbrake_test.go
+++ airbrake_test.go
@@ -11,9 +11,9 @@ import (
 func TestError(t *testing.T) {
 	Verbose = true
 	ApiKey = ""
-	Endpoint = "https://exceptions.shopify.com/notifier_api/v2/notices.xml"
+	Endpoint = "https://api.airbrake.io/notifier_api/v2/notices"
 
-	err := Error(errors.New("GenericFailure"), nil)
+	err := Error(errors.New("Test Error"), nil)
 	if err != nil {
 
 		t.Error(err)
@@ -25,12 +25,26 @@ func TestError(t *testing.T) {
 func TestRequest(t *testing.T) {
 	Verbose = true
 	ApiKey = ""
-	Endpoint = "https://exceptions.shopify.com/notifier_api/v2/notices.xml"
+	Endpoint = "https://api.airbrake.io/notifier_api/v2/notices"
 
 	request, _ := http.NewRequest("GET", "/some/path?a=1", bytes.NewBufferString(""))
 
-	err := Error(errors.New("GenericFailure"), request)
+	err := Error(errors.New("Test Error"), request)
 
+	if err != nil {
+		t.Error(err)
+	}
+
+	time.Sleep(1e9)
+}
+
+func TestNotify(t *testing.T) {
+	Verbose = true
+	ApiKey = ""
+	Endpoint = "https://api.airbrake.io/notifier_api/v2/notices"
+	
+	err := Notify(errors.New("Test Error")
+	
 	if err != nil {
 		t.Error(err)
 	}
